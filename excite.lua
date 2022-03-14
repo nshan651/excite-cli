78faa810a5383250aa1aee5998c59dca178f006b
nshan651 <nickesg44@gmail.com>
2022-03-14 16:34:42 -0500

    Redefined format.lua to be a class

--- excite.lua
+++ excite.lua
@@ -1,10 +1,12 @@
--- excite.lua
+--[[
+    excite.lua
+--]]
 
 -- Requirements
 local json = require "JSON"
 local curl = require "cURL"
 local argparse = require "argparse"
-local fmt = require "./format"
+local Format = require "./format"
 
 local ascii_logo = [[
     ███████╗██╗  ██╗ ██████╗██╗████████╗███████╗
@@ -17,9 +19,9 @@ local ascii_logo = [[
 
 -- Define arguments
 local parser = argparse("excite", ascii_logo)
---parser:argument("input", "ISBN code")
+parser:argument("input", "ISBN code")
 parser:argument("cite_style")
---    :choices {"bibtex", "APA", "MLA"}
+    :choices {"bibtex", "APA", "MLA"}
 parser:flag("-o --output", "Output citation to a file.")
 parser:option("-r --rename", "Rename output file.", "citation.txt")
 
@@ -30,12 +32,13 @@ STYLE = ARGS.cite_style
 OUTPUT = ARGS.output
 DEFAULT_FILE = ARGS.rename
 
-local isbn = "9780262033848" --"0140286802"
---local worksid = "OL23170657M" --"OL45883W" 
 local filename = "/home/nick/github_repos/excite-cli/data.txt"
---local url = string.format("https://openlibrary.org/works/%s.json", worksid)
-local url = "https://openlibrary.org/api/books?bibkeys=ISBN:" .. isbn .. "&jscmd=data&format=json"
 
+--[[
+    Some example ISBNs:
+    local isbn = "9781590171332" --"9780262033848" --"0140286802"
+    local worksid = "OL23170657M" --"OL45883W"
+--]]
 
 -- Split function (no string split function in standard lua)
 local function split(inputstr, sep)
@@ -53,13 +56,11 @@ end
 local function decode(url)
     local f = io.open(filename, "w")
     local c = curl.easy_init()
-        -- setup url
         c:setopt_url(url)
         -- perform, invokes callbacks
         c:perform({writefunction = function(str)
                         f:write(str)
                         end})
-        -- close output file
         f:close()
     local data = io.open(filename)
     local str = data:read("*a")
@@ -67,45 +68,19 @@ local function decode(url)
     return json:decode(str)
 end
 
---[[
--- Bibtex format
-local function bibtex(decode, authors, title, year, publisher)
-    -- Format id: Split the author string, loop through to find last name, format id
-    local first_author = split(decode["ISBN:" .. isbn]["authors"][1]["name"], "%s")
-    local id = nil
-    for _,s in pairs(first_author) do; id=s; end
-    id = id:lower() .. year
-    -- Return a bibtex string
-    return "@book{" .. id .. ",\n" ..
-    "author = " .. string.format("\"%s\"", authors) .. "\n" ..
-    "title = " .. string.format("\"%s\"", title) .. "\n" ..
-    "year = " .. string.format("\"%s\"", year) .. "\n" ..
-    "publisher = " .. string.format("\"%s\"", publisher) .. "\n" ..
-    "}"
-end
-
-local function mla(authors, title, container, edition, publisher, year)
-    return authors .. ". \"" .. title .. ".\" \n\t" .. edition .. ", " .. publisher .. ", \t" .. year .. ", Accessed 08 March 2022."
-end
-
-local function apa()
-end
-
---]]
-
 local function cite(decode, url)
     -- Gather basic info: author, title, year, publisher
     print("Fetching Citation...")
     local isbn_data = decode(url)
 
     -- Get edition from worksID
-    local OLID = isbn_data["ISBN:" .. isbn]["identifiers"]["openlibrary"][1]
+    local OLID = isbn_data["ISBN:" .. INPUT]["identifiers"]["openlibrary"][1]
     local url2 = "https://openlibrary.org/books/" .. OLID .. ".json"
     local edition = decode(url2)["edition_name"]
 
     -- Handle authors
-    local names = isbn_data["ISBN:" .. isbn]["authors"]
-    local authors = isbn_data["ISBN:" .. isbn]["authors"][1]["name"]
+    local names = isbn_data["ISBN:" .. INPUT]["authors"]
+    local authors = isbn_data["ISBN:" .. INPUT]["authors"][1]["name"]
     for i,v in pairs(names) do
         if (i > 1) then
             authors = authors .. ", " .. v["name"]
@@ -114,32 +89,31 @@ local function cite(decode, url)
 
     -- Split date string to find the year
     local year = nil
-    local date = split(isbn_data["ISBN:" .. isbn]["publish_date"], "%s")
+    local date = split(isbn_data["ISBN:" .. INPUT]["publish_date"], "%s")
     for _,s in pairs(date) do; year=s; end
     -- Title and publisher
-    local title = isbn_data["ISBN:" .. isbn]["title"]
-    local publisher = isbn_data["ISBN:" .. isbn]["publishers"][1]["name"]
+    local title = isbn_data["ISBN:" .. INPUT]["title"]
+    local publisher = isbn_data["ISBN:" .. INPUT]["publishers"][1]["name"]
 
+    -- Declare output and Format object
     local output = nil
+    local fmt = Format:new(authors, title, nil, nil, year, publisher)
 
-    -- test fmt
-    --output = fmt.bibtex(id, authors, title, year, publisher)
-    --output = fmt.mla(authors, title, nil, edition, publisher, year)
-
+    -- Choose a citation style
     if STYLE == "bibtex" then
-        print("HERE")
         -- Format id
-        local first_author = split(isbn_data["ISBN:" .. isbn]["authors"][1]["name"], "%s")
+        local first_author = split(isbn_data["ISBN:" .. INPUT]["authors"][1]["name"], "%s")
         local id = nil
         for _,s in pairs(first_author) do; id=s; end
         id = id:lower() .. year
         print("Bibtex selected")
-        output = fmt.bibtex(id, authors, title, year, publisher)
+        output = fmt:bibtex(id)
     elseif STYLE == "MLA" then
         print("MLA Selected")
         output = fmt.mla(authors, title, nil, edition, publisher, year)
     elseif STYLE == "APA" then
         print("APA Selected")
+        output = fmt.apa(authors, title, nil, edition, publisher, year)
     end
 
     assert(type(output) == "string", "Failed to format citation")
@@ -148,7 +122,7 @@ local function cite(decode, url)
     f:close()
     --os.execute(string.format("echo \"%s\" | xclip -sel clip", output))
     os.execute(string.format("cat output.txt | xclip -sel clip", output))
-    -- os.execute("viu test3.jpg")
+    --os.execute("viu test3.jpg")
 
     ---[[
     print("---------------------------------------------------------")
@@ -158,6 +132,32 @@ local function cite(decode, url)
     print("Citation copied to clipboard")
 end
 
+local url = nil
+-- Pattern matching query to check for ISBN 10/13
+if string.match(INPUT, "%d%d%d%d%d%d%d%d%d%d%d%d%d") then
+    -- ISBN search
+    url = "https://openlibrary.org/api/books?bibkeys=ISBN:" .. INPUT .. "&jscmd=data&format=json"
+else
+    -- Text-based search
+    local query = split(string.lower(INPUT))
+    local output = query[1]
+    for i,_ in pairs(query) do
+        if i > 1 then
+            output = output .. "+" .. query[i]
+        end
+    end
+    url = "http://openlibrary.org/search.json?title=" .. output
+end
 
 cite(decode, url)
 
+-- TEST
+--[[
+local d = decode(url)
+
+local tab = {"yeezy", "MBDTF", nil, "69th Edition", "GOOD Music", "1999"}
+local form = fmt:new("yeezy", "MBDTF", nil, "69th Edition", "GOOD Music", "1999")
+
+--form.tab = tab
+print(fmt:bibtex("ye420"))
+--]]
--- format.lua
+++ format.lua
@@ -1,26 +1,85 @@
--- Citation Formatting
+--[[
+    Citation formatting class for bibtex, MLA, and APA
+--]]
 
-local format = {}
+local Format = {
+    authors = "",
+    title = "",
+    container = "",
+    edition = "",
+    publisher = "",
+    year = ""
+}
+
+-- Constructor
+function Format:new(authors, title, container, edition, publisher, year)
+    o = {}
+    setmetatable(o, self)
+    self.__index = self
+    self.authors = authors
+    self.title = title
+    self.container = container
+    self.edition = edition
+    self.publisher = publisher
+    self.year = year
+    return o
+end
 
 -- Bibtex format
-function format.bibtex(id, authors, title, year, publisher)
-    -- Format id: Split the author string, loop through to find last name, format id
-    -- Return a bibtex string
+-- Format id: Split the author string, loop through to find last name, format id
+-- Return a bibtex string
+function Format:bibtex(id)
     return "@book{" .. id .. ",\n" ..
-    "author = " .. string.format("\"%s\"", authors) .. "\n" ..
-    "title = " .. string.format("\"%s\"", title) .. "\n" ..
-    "year = " .. string.format("\"%s\"", year) .. "\n" ..
-    "publisher = " .. string.format("\"%s\"", publisher) .. "\n" ..
+    "author = " .. string.format("\"%s\"", self.authors) .. "\n" ..
+    "title = " .. string.format("\"%s\"", self.title) .. "\n" ..
+    "year = " .. string.format("\"%s\"", self.year) .. "\n" ..
+    "publisher = " .. string.format("\"%s\"", self.publisher) .. "\n" ..
     "}"
 end
 
+--[[
+-- MLA Citation
 function format.mla(authors, title, container, edition, publisher, year)
-    return authors .. ". \"" .. title .. ".\" \n\t" .. edition .. ", " .. publisher .. ", \t" .. year .. ", Accessed 08 March 2022."
+    local tab = {authors .. ". \"", title .. ".\" ", publisher .. ", ", year .. "."}
+    if container then
+        table.insert(tab, 3, container)
+    end
+    if edition then
+        table.insert(tab, 4, edition)
+    end
+    local new_line = true
+    local output = ""
+    for i,_ in pairs(tab) do
+        if string.len(output) > 80 and new_line then
+            output = output .. "\n\t"
+            new_line = false
+        end
+       output = output .. tab[i]
+    end
+    return output
 end
 
-function format.apa()
-    print("APA selected")
+function format.apa(authors, title, container, edition, publisher, year)
+    publisher = string.upper(publisher)
+    local tab = {authors, "(" .. year .. "). ", title .. ". ", publisher .. "."}
+    if container then
+        table.insert(tab, 3, container)
+    end
+    if edition then
+        table.insert(tab, 4, edition)
+    end
+    local new_line = true
+    local output = ""
+    for i,_ in pairs(tab) do
+        if string.len(output) > 80 and new_line then
+            output = output .. "\n\t"
+            new_line = false
+        end
+       output = output .. tab[i]
+    end
+    return output
 end
+--]]
 
-return format
+return Format
 
